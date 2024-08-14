5f705983163e496961797efe75efa36c51bdf3e6
A Beetle <abeetle15@gmail.com>
2024-08-14 14:16:23 -0500

    Add style.
    add sticky header with a basket icon that takes user to the cart page

--- site-functions.js
+++ site-functions.js
@@ -1,9 +1,18 @@
-import { addToCart } from "./cart-functions"
+import { addToCart, cart, currentTotal, findCurrent } from "./cart-functions.js"
 
 export function addEventListeners(list) {
   for (let i = 0; i < list.length; i++) {
     list[i].addEventListener('click', ()=> {
-      addToCart(i)
+      addToCart(i);
+      showCart();
     })
   }
+}
+
+export function showCart() {
+  console.clear();
+  console.log('Cart:')
+  console.log(cart)
+  findCurrent(cart)
+  console.log(`Current total: ${currentTotal}`)
 }
